5129292ac42861dad01794808d0ee3387ce7cecb
OlehKapat <bombastik@ukr.net>
2026-06-05 16:33:15 +0300

    initial commit

---
+++ schemas.py
@@ -0,0 +1,45 @@
+from pydantic import BaseModel, ValidationError, Field, ConfigDict,EmailStr
+from datetime import datetime
+
+
+class UserBase(BaseModel):
+    username: str = Field(min_length=3, max_length=50)
+    email: EmailStr = Field( max_length=120)
+
+class UserCreate(UserBase):
+    pass
+
+class UserResponse(UserBase):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    image_path: str 
+    image_file: str | None
+
+class UserUpdate(BaseModel):
+    username: str | None = Field(default=None,  min_length=3, max_length=50)
+    email: EmailStr | None = Field(default=None, max_length=120)
+    image_file: str | None = Field(default=None,min_length=3, max_length=200)
+
+
+class PostUpdate(BaseModel):
+     title: str | None = Field(default=None, min_length=2, max_length=100)
+     content: str | None = Field(default=None, min_length=1)
+
+class PostBase(BaseModel):
+    title: str = Field(min_length=2, max_length=100)
+    content: str = Field(min_length=1)
+
+class PostCreate(PostBase):
+    user_id: int
+
+
+class PostResponse(PostBase):
+    model_config = ConfigDict(from_attributes=True)
+    id: int
+    user_id: int
+    date_posted: datetime
+    author: UserResponse
+
+
+
+    id: int
